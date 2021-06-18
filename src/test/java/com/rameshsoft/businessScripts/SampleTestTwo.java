15e95589d3c094a9d6bb92d4e24c7a2c3c55387f
Anitha11421 <anutechmyself12@gmail.com>
2021-06-18 17:40:13 +0530

    Added SampleTestTwo file

---
+++ src/test/java/com/rameshsoft/businessScripts/SampleTestTwo.java
@@ -0,0 +1,27 @@
+package com.rameshsoft.businessScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTestTwo {
+	@Test
+	public static void testtwo() throws InterruptedException
+	{
+		
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			System.out.println("browser lunched susccessfully");
+			driver.manage().window().maximize();
+			System.out.println("maximise the window");
+			driver.get("https://www.flipkart.com");
+			System.out.println("Navigaed to : "+driver.getCurrentUrl());
+			
+			Thread.sleep(2000);
+			driver.close();
+			
+		}
+	}
+
